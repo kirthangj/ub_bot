5fead37549cb83d0a75cb63fd4181d80b5c597cc
kirthan <kirthangj023@gmail.com>
2016-08-04 01:08:22 +0530

    second

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 $botToken = "215663608:AAFFBj8OXQ2HbT3Cj2lmydwVeP4V2CDOaOo";
 $website = "https://api.telegram.org/bot".$botToken;
-//$update = file_get_contents($website."/getupdates");
+$update = file_get_contents($website."/getupdates");
 //print_r($update);
-$update = file_get_contents("php://input");
+//$update = file_get_contents("php://input");
 print_r($update);
 $updatevar = json_decode($update,TRUE);
 print_r($updatevar);
