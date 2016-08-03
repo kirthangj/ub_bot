becd338fd3585ebf9a8ea5fd781419ab68eada4d
kirthan <kirthangj023@gmail.com>
2016-08-04 01:33:42 +0530

    third

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 $botToken = "215663608:AAFFBj8OXQ2HbT3Cj2lmydwVeP4V2CDOaOo";
 $website = "https://api.telegram.org/bot".$botToken;
-$update = file_get_contents($website."/getupdates");
+//$update = file_get_contents($website."/getupdates");
 //print_r($update);
-//$update = file_get_contents("php://input");
+$update = file_get_contents("php://input");
 print_r($update);
 $updatevar = json_decode($update,TRUE);
 print_r($updatevar);
-$text = $updatevar["result"][8]["message"]["chat"]["id"];
+$text = $updatevar["result"][200]["message"]["chat"]["id"];
 file_get_contents($website."/sendmessage?chat_id=144983285&text=web hook");
-
+print_r($text);
 ?>
