bdf3cf4286848af12a3b86423ed5d608ca7c0e85
kirthan <kirthangj023@gmail.com>
2016-08-05 01:07:48 +0530

    6

--- index.php
+++ index.php
@@ -7,29 +7,31 @@ $website = "https://api.telegram.org/bot".$botToken;
 //print_r($update);
 $update = file_get_contents("php://input");
 print_r($update);
+var_dump($update);
 $update = json_decode($update,TRUE);
 print_r($update);
-$chat_id = $update["message"]["chat"]["id"];
-print_r($chat_id);
+$chatid = $update["message"]["chat"]["id"];
+var_dump($chatid);
 $messgae=$update["message"]["text"];
 switch($messgae){
 	case "/test":
-		sendmess($chat_id,"test");
+		sendmess($chatid,"test");
 		break;
 	case "/hi":
-		sendmess($chat_id,"hey there");
+		sendmess($chatid,"hey there");
 		break;
 	default:
-		sendmess($chat_id,"default");
+		sendmess($chatid,"default");
 
 	
 }
 //$text = $updatevar["result"][200]["message"]["chat"]["id"];
 
-function sendmess ($chat_id,$mess)
+function sendmess ($chatid,$mess)
 {
-    $url = $GLOBALS[website]."/sendMessage?chat_id=".$chat_id."&text".urlencode($mess);
-    file_get_contents($url);
+   $url = $GLOBALS["website"]."/sendmessage?chat_id=144983285&text=".urlencode($mess);
+   var_dump($url); 
+   file_get_contents($url);
 }    
 
 ?>
